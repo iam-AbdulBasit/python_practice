5ba0409d467aeb53e5e2b7da276491865a5a5e73
iam-AbdulBasit <120900763+iam-AbdulBasit@users.noreply.github.com>
2023-01-06 00:29:13 +0500

    Create Vowel.py

---
+++ Vowel.py
@@ -0,0 +1,14 @@
+vowel = input("enter a alphabet")
+match vowel:
+    case "a":
+        print("it is vowel")
+    case "e":
+        print("it is vowel")
+    case "i":
+        print("it is vowel")
+    case "o":
+        print("it is vowel")
+    case "u":
+        print("it is vowel")
+    case _:
+        print("it is not vowel")
